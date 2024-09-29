7336cbaa01f1e29cd5dfcc5753b89b16b0a7ecdd
aehamdy <ae.hamdy91@gmail.com>
2024-09-29 17:16:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        className="h-screen bg-no-repeat bg-center"
+        style={{ backgroundImage: "url('/starry-night.png')" }}
+      ></div>
+    </>
+  );
+}
+
+export default App;
