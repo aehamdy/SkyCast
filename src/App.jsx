83ffe471257ba0c06d357db5c0ab8e0caa26a807
aehamdy <ae.hamdy91@gmail.com>
2024-09-30 17:24:30 +0300

    chore: Added app background and House image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
+import House from "../public/house.png";
 
 function App() {
   return (
     <>
       <div
-        className="h-screen bg-no-repeat bg-center"
+        className="realtive h-screen bg-no-repeat bg-center"
         style={{ backgroundImage: "url('/starry-night.png')" }}
-      ></div>
+      >
+        <img
+          src={House}
+          alt="house-image"
+          className="absolute w-2/5 top-1/2 start-1/2 -translate-y-1/4 -translate-x-1/2"
+        />
+      </div>
     </>
   );
 }
