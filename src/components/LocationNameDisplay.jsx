69479acfba252cb0be7b73e8453428c415a8eaad
aehamdy <ae.hamdy91@gmail.com>
2024-09-30 18:06:34 +0300

    chore: Changed TownName component into LocationNameDisplay for better name discription

---
+++ src/components/LocationNameDisplay.jsx
@@ -0,0 +1,5 @@
+function LocationNameDisplay() {
+  return <h3>Town&apos;s Name</h3>;
+}
+
+export default LocationNameDisplay;
--- src/components/Town.jsx
+++
@@ -1,5 +0,0 @@
-function TownName() {
-  return <h3>Town's Name</h3>;
-}
-
-export default TownName;
