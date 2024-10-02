33c8b16fec23eccd46290a9c4063584440930060
aehamdy <ae.hamdy91@gmail.com>
2024-10-02 23:53:46 +0300

    feat: Handle and display the proper weather image depending on weatherDescription prop

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-function WeatherIcon() {
+function WeatherIcon({ weatherDescription }) {
   const sunny = "/weather-sunny.png";
   const foggy = "/weather-foggy.png";
   const rainy = "/weather-rain.png";
@@ -9,9 +9,45 @@ function WeatherIcon() {
   const windy = "/weather-wind.png";
   const tornado = "/weather-tornado.png";
 
+  const handleWeatherIcon = () => {
+    switch (weatherDescription.toLowerCase()) {
+      case "clear":
+        return sunny;
+      case "fog":
+      case "mist":
+      case "smoke":
+        return foggy;
+      case "rain":
+      case "drizzle":
+      case "light rain":
+      case "moderate rain":
+      case "heavy intensiy rain":
+        return rainy;
+      case "clouds":
+      case "broken clouds":
+      case "overcast clouds":
+      case "scattered clouds":
+        return cloudy;
+      case "snow":
+      case "light snow":
+      case "heavy snow":
+        return snowy;
+      case "thunderstorm":
+        return stormy;
+      case "wind":
+        return windy;
+      case "tornado":
+        return tornado;
+      default:
+        return null;
+    }
+  };
+
+  const weatherIcon = handleWeatherIcon();
+
   return (
     <div>
-      <img src="" alt="" />
+      <img src={weatherIcon} alt="weather image" width="70" />
     </div>
   );
 }
