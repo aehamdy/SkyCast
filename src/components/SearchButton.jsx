f56a1b46e4b54d4a5f563870850320d373012423
aehamdy <ae.hamdy91@gmail.com>
2024-10-02 20:35:58 +0300

    feat: Add handleButtonClick functionality to SearchButton

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,7 +1,13 @@
-function SearchButton() {
+/* eslint-disable react/prop-types */
+function SearchButton({ validateInputValue }) {
+  const handleButtonClick = () => {
+    validateInputValue();
+  };
+
   return (
     <button
       type="button"
+      onClick={handleButtonClick}
       className="absolute end-0 top-0 h-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white px-2.5 rounded-e-xl outline-none"
     >
       Search
