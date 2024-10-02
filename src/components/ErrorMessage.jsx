e975853db19c0d350f5794a8f83fb7857ebae751
aehamdy <ae.hamdy91@gmail.com>
2024-10-02 23:59:57 +0300

    style: Change the text color of error message

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function ErrorMessage({ errorMessage }) {
   return (
-    <p className="text-white">
+    <p className="text-red-300">
       {errorMessage.split("\n").map((line, index) => (
         <span key={index}>
           {line}
