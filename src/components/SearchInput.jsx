4858277a1ac38d3d550f608844ec5e2cb76be0e2
aehamdy <ae.hamdy91@gmail.com>
2024-10-02 20:34:06 +0300

    feat: SearchInput handles and responsible for input changes

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,14 @@
-function SearchInput() {
+/* eslint-disable react/prop-types */
+function SearchInput({ setInputValue }) {
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <input
       type="search"
       name="search-input"
+      onChange={handleInputValue}
       className="w-full py-2 ps-2 pe-[4.2rem] rounded-xl outline-none"
     />
   );
