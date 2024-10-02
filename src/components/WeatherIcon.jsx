e49abe7e1d513516ce6e305ab283426b3f605242
aehamdy <ae.hamdy91@gmail.com>
2024-10-02 23:52:35 +0300

    chore: Added weather icons

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,5 +1,14 @@
 /* eslint-disable react/prop-types */
 function WeatherIcon() {
+  const sunny = "/weather-sunny.png";
+  const foggy = "/weather-foggy.png";
+  const rainy = "/weather-rain.png";
+  const cloudy = "/weather-cloudy.png";
+  const snowy = "/weather-snow.png";
+  const stormy = "/weather-storm.png";
+  const windy = "/weather-wind.png";
+  const tornado = "/weather-tornado.png";
+
   return (
     <div>
       <img src="" alt="" />
