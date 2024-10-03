f38b3082dae52f89d39eb6efdb7947b3fe108db1
aehamdy <ae.hamdy91@gmail.com>
2024-10-03 06:12:17 +0300

    feat: Created NextDayCard to display each day weather

---
+++ src/components/NextDayCard.jsx
@@ -0,0 +1,11 @@
+function NextDayCard() {
+  return (
+    <li className="flex flex-col gap-2">
+      <p>date</p>
+      <img src="" alt="" />
+      <p>weather description</p>
+    </li>
+  );
+}
+
+export default NextDayCard;
