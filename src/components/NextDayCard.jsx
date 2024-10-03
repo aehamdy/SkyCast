1a50dbea5a6860734f3526fcf75168f7d9fd1def
aehamdy <ae.hamdy91@gmail.com>
2024-10-03 07:18:03 +0300

    feat: Added a function to handle weather icon depending on received description

--- src/components/NextDayCard.jsx
+++ src/components/NextDayCard.jsx
@@ -1,4 +1,13 @@
 /* eslint-disable react/prop-types */
+const sunny = "/weather-sunny.png";
+const foggy = "/weather-foggy.png";
+const rainy = "/weather-rain.png";
+const cloudy = "/weather-cloudy.png";
+const snowy = "/weather-snow.png";
+const stormy = "/weather-storm.png";
+const windy = "/weather-wind.png";
+const tornado = "/weather-tornado.png";
+
 function NextDayCard({ date, temp, description }) {
   const formattedDate = () => {
     const fullTime = date.split(" ");
@@ -15,13 +24,48 @@ function NextDayCard({ date, temp, description }) {
     return value;
   };
 
+  const handleWeatherIcon = () => {
+    switch (description.toLowerCase()) {
+      case "clear":
+        return sunny;
+      case "fog":
+      case "mist":
+      case "smoke":
+        return foggy;
+      case "rain":
+      case "drizzle":
+      case "light rain":
+      case "moderate rain":
+      case "heavy intensiy rain":
+        return rainy;
+      case "clouds":
+      case "broken clouds":
+      case "overcast clouds":
+      case "scattered clouds":
+        return cloudy;
+      case "snow":
+      case "light snow":
+      case "heavy snow":
+        return snowy;
+      case "thunderstorm":
+        return stormy;
+      case "wind":
+        return windy;
+      case "tornado":
+        return tornado;
+      default:
+        return null;
+    }
+  };
+
   const nextDayDate = formattedDate();
+  const weatherIcon = handleWeatherIcon();
   const nextDayTemp = formatTemp();
 
   return (
     <li className="flex flex-col gap-2">
       <p>{nextDayDate}</p>
-      <img src="" alt="" />
+      <img src={weatherIcon} alt="weather image" width="17" />
       <p>{nextDayTemp}</p>
     </li>
   );
